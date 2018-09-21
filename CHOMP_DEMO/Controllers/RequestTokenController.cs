80edec2eb910a44ea58cde273931d4f811cd9235
Jesus <dolpsdw@gmail.com>
2018-09-21 16:49:37 +0200

    jwtEmitter

---
+++ CHOMP_DEMO/Controllers/RequestTokenController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CHOMP_DEMO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestTokenController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost]  //Frombody-> deserializa los argumentos pasados a la api
+        public IActionResult RequestToken([FromBody] TokenRequest request)
+        {
+            if (request.username == "1234" && request.contra == "1234")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.username)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("LARGESYMETRICCHUNKOFSTRINGMAYORTHAN128BITSOFLENGHTFORSHAREINSERVERS"/*_configuration["SecurityKey"]*/));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "chomp.chain",
+                    audience: "tiggergaming.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+
+        public class TokenRequest
+        {
+            public string username;
+            public string contra;
+        }
+
+    }
+}
